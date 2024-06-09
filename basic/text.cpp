894ac7977dedbfae8960f0ecfddd904d6659db5b
Mahedi205339 <mahedi205339@gmail.com>
2024-06-10 01:15:54 +0600

    c++ compiler update of vs code problem solved

--- basic/text.cpp
+++ basic/text.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <conio.h>
+
+using namespace std ;
+
 int main()
 {
-    std::cout << "Hello , World " << std::endl;
-    return 0;
+   cout<< "Mahedi " << " Hasan";
+
+   getch();
 }
