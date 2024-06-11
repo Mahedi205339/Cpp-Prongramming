a42c1e42b2aa6f160ef71344d9db13ba0102778b
Mahedi205339 <mahedi205339@gmail.com>
2024-06-11 23:31:44 +0600

    added some test code

--- basic/text.cpp
+++ basic/text.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <conio.h>
 
-using namespace std ;
+using namespace std;
 
 int main()
 {
-   cout<< "Mahedi " << " Hasan";
+   cout << "Mahedi " << " Hasan" << endl
+        << "Dhaka \n"
+        << "045404309 \n";
 
    getch();
 }
---
+++ basic/text2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+
+    num1 = 10;
+    num2 = 23;
+
+    cout <<"Number 1= "<< num1 << endl;
+    cout <<"Number 2= "<< num2 << endl;
+
+    getch();
+}
