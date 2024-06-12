eb7fde762042657cc63c66c84f19a1dbb1bda623
Mahedi205339 <mahedi205339@gmail.com>
2024-06-12 23:16:19 +0600

    added input fundamentals

--- basic/intInput.cpp
+++ basic/intInput.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <conio.h>
+// using conio.h for getch()
 
 using namespace std;
 
 int main()
 {
-    char name[13] = "Mahedi Hasan";
-    cout << "My name is " << name;
+    int num, num1;
+    cout << "Enter two  number :";
+    cin >> num;
+    cin >> num1;
+
+
+    cout << "Entered number are :" << num << endl;
+    cout << "Entered number are :" << num1 << endl;
 
     getch();
 }
