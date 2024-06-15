aa13145a677a4010332e9d104f8c2c5f53475b38
Mahedi205339 <mahedi205339@gmail.com>
2024-06-15 09:03:12 +0600

    added fixed, setprecision,stw,showpoint noshowpoint functions

--- basic/calc.cpp
+++ basic/calc.cpp
@@ -1,36 +1,44 @@
 #include <iostream>
 #include <conio.h>
+#include <iomanip>
 
 using namespace std;
 int main()
 {
-    int num1, num2;
+    float num1, num2;
 
     // num1 = 25;
     // num2 = 10;
-    cout<< "Enter first number : ";
-    cin>> num1;
-    cout<< "Enter second number : ";
-    cin>> num2;
+    cout << "Enter first number : ";
+    cin >> num1;
+    cout << "Enter second number : ";
+    cin >> num2;
 
-    int sum = num1 + num2;
-    cout << "Sum is : " << sum;
+    cout << showpoint;
+    cout << fixed;
+    cout << setprecision(2);
+
+    float sum = num1 + num2;
+    cout << setw(25) << "Sum is : " << sum;
+    cout << endl;
+    // cout << noshowpoint;
+    float sub = num1 - num2;
+    cout << setw(25) << "Sub is : " << sub;
     cout << endl;
 
-    int sub = num1 - num2;
-    cout << "Sub is : " << sub;
+    float mul = num1 * num2;
+    cout  << setw(25)<< "Mul is : " << mul;
     cout << endl;
 
-    int mul = num1 * num2;
-    cout << "Mul is : " << mul;
+    double div = float(num1) / num2; // type casting
+    cout<< setw(25) << "Div is : " << div;
     cout << endl;
 
-    double div = float(num1) / num2; //type casting
-    cout << "Div is : " << div;
-    cout << endl;
-    int rem = num1 % num2;
-    cout << "Rem is : " << rem;
-    cout << endl;
+    // float data type does not allow to take reminder.only integer allows
+
+    // float rem = num1 % num2;
+    // cout << "Rem is : " << rem;
+    // cout << endl;
 
     getch();
 }
