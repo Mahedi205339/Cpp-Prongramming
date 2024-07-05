fe37a2ac429cd01e16b966a1aebdd9135e91ae92
Mahedi205339 <mahedi205339@gmail.com>
2024-07-05 21:41:48 +0600

    added pythagrean triplet

---
+++ advance/problem1.cpp
@@ -0,0 +1,24 @@
+/*
+  sum of 1 to n th num ;
+ */
+
+#include <iostream>
+using namespace std;
+
+int sum(int n)
+{
+    int ans = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        ans += i;
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    cout << sum(n) << endl;
+}
