a6e5009cf66c210a4ce9a1205582aec160f5dffe
Mahedi205339 <mahedi205339@gmail.com>
2024-10-01 00:55:17 +0600

    adde array obj

--- class/arrobj.cpp
+++ class/arrobj.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Shop
+class ShopItem
 {
     int if;
     float price;
@@ -18,8 +18,32 @@ public:
         cout << "Price of this item is " << price << endl;
     }
 };
-int main(){
-    
+int main()
+{
+    int p, q;
+    int size = 2;
+    ShopItem *ptr = new ShopItem[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter Id and price of item " << i + 1 << endl;
+        cin >> p >> q;
+
+        // dereference the pointer
+        // both method are same here
+
+        // (*ptr).setData(p, q);
+        ptr->setData(p, q);
+        ptr++;
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+
+        cout << "Item number: " << i + 1 << endl;
+        ptr->getData();
+        ptr++;
+    }
 
     return 0;
 }
