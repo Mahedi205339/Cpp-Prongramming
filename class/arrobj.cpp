209e3b260c4d5441ffa8091fc7c9bc25ad1034b3
Mahedi205339 <mahedi205339@gmail.com>
2024-09-30 23:57:14 +0600

    aded array object code snippet

---
+++ class/arrobj.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Shop
+{
+    int if;
+    float price;
+
+public:
+    void setData(int a, float b)
+    {
+        id = a;
+        price = b;
+    }
+    void getData(void)
+    {
+        cout << "Code of this item is " << id << endl;
+        cout << "Price of this item is " << price << endl;
+    }
+};
+int main(){
+    
+
+    return 0;
+}
