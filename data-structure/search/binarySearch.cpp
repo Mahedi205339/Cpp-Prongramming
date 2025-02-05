ae931f62001ac40aa5cb9974251182fb484a5e0d
Mahedi205339 <mahedi205339@gmail.com>
2025-02-05 22:09:13 +0600

    added data-struchture binary search

--- data-structure/search/binarySearch.cpp
+++ data-structure/search/binarySearch.cpp
@@ -3,5 +3,42 @@ using namespace std;
 
 int main()
 {
-    
+    int array[100], search, i, n, first, middle, last;
+
+    cout << "Enter number of elements in array: ";
+    cin >> n;
+
+    cout << "Enter " << n << " integers in sorted order: " << endl;
+    for (i = 0; i < n; i++)
+    {
+        cin >> array[i];
+    }
+
+    cout << "Enter a number to search: ";
+    cin >> search;
+
+    first = 0;
+    last = n - 1;
+
+    while (first <= last)
+    {
+        middle = (first + last) / 2; // Corrected middle calculation
+
+        if (array[middle] == search)
+        {
+            cout << search << " found at location " << middle + 1 << endl;
+            return 0;
+        }
+        else if (array[middle] < search)
+        {
+            first = middle + 1; // Search in right half
+        }
+        else
+        {
+            last = middle - 1; // Search in left half
+        }
+    }
+
+    cout << search << " is not in the array." << endl;
+    return 0;
 }
